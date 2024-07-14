c7d4d7948308bb01e42d97b26894df3a089be86e
Vinícius Santos <vfssantos@outlook.com>
2024-07-14 14:09:13 -0300

    Update login.ts

--- api/login.ts
+++ api/login.ts
@@ -12,7 +12,7 @@ export const POST = async ({ username, password }) => {
   const user = users.find((u) => u.username === username && u.password === password);
 
   if (!user) {
-    return { status: 401, body: { message: "Invalid username or password" } };
+    throw { status: 401, body: { message: "Invalid username or password" } };
   }
 
   const jwt = await create({ alg: "HS512", typ: "JWT" }, { username }, key);
